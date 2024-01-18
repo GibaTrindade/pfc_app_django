d0f8006fccea7ff64b380f16f55b67f1eaa2f6bd
Gilberto <g.trindade@gmail.com>
2024-01-18 20:24:45 -0300

    layout de carga horaria

--- pfc_app_django/settings.py
+++ pfc_app_django/settings.py
@@ -166,13 +166,13 @@ MESSAGE_TAGS = {
 }
 
 # Configurações de E-mail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ncdseplag@gmail.com'
-EMAIL_HOST_PASSWORD = 'jworzvpvvsguxzfu'
-DEFAULT_FROM_EMAIL = 'ncdseplag@gmail.com'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 
 
