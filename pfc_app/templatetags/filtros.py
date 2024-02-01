40481360e0b5701fa0e2cb4e7877634d347e3e41
gilberto.nunes <g.trindade@gmail.com>
2024-02-01 14:14:30 -0300

    criacao de filtro de dividir

--- pfc_app/templatetags/filtros.py
+++ pfc_app/templatetags/filtros.py
@@ -12,3 +12,15 @@ def custom_pluralize(value, arg='s'):
         return arg
     else:
         return ''
+
+
+@register.filter(name='dividir')
+def dividir(value, arg=60):
+    try:
+        value = int(value)
+    except:
+        return ''
+    if value >= 0:
+        return str(round(value / arg * 100, 2))+'%'
+    else:
+        return ''
