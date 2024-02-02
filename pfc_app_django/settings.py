82178d7a281df0399c344ef8a9bc0cd4ae0eb969
gilberto.nunes <g.trindade@gmail.com>
2024-02-02 11:44:09 -0300

    mudanca de EMAIL_HOST

--- pfc_app_django/settings.py
+++ pfc_app_django/settings.py
@@ -172,7 +172,7 @@ MESSAGE_TAGS = {
 
 # Configurações de E-mail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ncdseplag@gmail.com'
